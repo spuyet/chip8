152d4c1029f117d849a1696e3be4ed3ab32d839b
Sebastien Puyet <sebastien@puyet.fr>
2019-01-19 18:51:35 +0100

    clear containers at file loading time

--- rust/src/chip8.rs
+++ rust/src/chip8.rs
@@ -1,10 +1,12 @@
 use std::io::prelude::*;
 use std::fs::File;
+use std::fs;
+use std::ptr;
 
-const REGISTER_COUNT: usize = 16;
+const REGISTER_COUNT : usize = 16;
 const MEMORY_SIZE : usize = 4096;
 const SCREEN_HEIGHT : usize = 32;
-const STACK_SIZE: usize = 16;
+const STACK_SIZE : usize = 16;
 
 pub struct Chip8 {
     registers: [u8; REGISTER_COUNT],
@@ -14,7 +16,6 @@ pub struct Chip8 {
     delay_timer: u8,
     sound_timer: u8,
     opcode: u16,
-    game: Vec<u8>,
     key: u8,
     I: u16,
     PC: u16,
@@ -28,19 +29,38 @@ impl Chip8 {
             memory: [0; MEMORY_SIZE],
             screen: [0; SCREEN_HEIGHT],
             stack: [0; STACK_SIZE],
-            opcode: 0, game: vec![],
+            opcode: 0,
             delay_timer: 0, sound_timer: 0,
             I: 0, PC: 0, SP: 0, key: 0,
         }
     }
 
     pub fn load_file(&mut self, filename: &str) -> std::io::Result<()> {
+        let len = fs::metadata(filename)?.len() as usize;
         let mut f = File::open(filename)?;
-        f.read_to_end(&mut self.game)?;
+
+        self.clear();
+        f.read_exact(&mut self.memory[0x200..(0x200 + len)])?;
         Ok(())
     }
 
     pub fn play(&self) {
 
+    }
+
+    fn clear(&mut self) {
+        unsafe {
+            let reg_ptr = self.registers.as_mut_ptr();
+            ptr::write_bytes(reg_ptr, 0, REGISTER_COUNT);
+
+            let mem_ptr = self.memory.as_mut_ptr();
+            ptr::write_bytes(mem_ptr, 0, MEMORY_SIZE);
+
+            let screen_ptr = self.screen.as_mut_ptr();
+            ptr::write_bytes(screen_ptr, 0, SCREEN_HEIGHT);
+
+            let stack_ptr = self.stack.as_mut_ptr();
+            ptr::write_bytes(stack_ptr, 0, STACK_SIZE);
+        }
     }
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::process;
 
 mod chip8;
+use self::chip8::Chip8;
 
 fn main() {
     let args : Vec<String> = env::args().collect();
@@ -9,7 +10,7 @@ fn main() {
         println!("A file has to be given as argument.");
         process::exit(0x0100);
     } else {
-        let mut chip = chip8::Chip8::new();
+        let mut chip = Chip8::new();
         let filename = &args[1];
         match chip.load_file(filename) {
             Ok(_) => chip.play(),
